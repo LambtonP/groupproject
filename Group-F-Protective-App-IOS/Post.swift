8bdae6af9a1a29b9b440938fcd1656f0d46f81d1
simranjeetkaur <chakkalsimmi@gmail.com>
2019-11-21 16:04:01 -0500

    login design

---
+++ Group-F-Protective-App-IOS/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  Group-F-Protective-App-IOS
+//
+//  Created by Simran Chakkal on 2019-11-21.
+//  Copyright © 2019 Simran Chakkal. All rights reserved.
+//
+
+import Foundation
