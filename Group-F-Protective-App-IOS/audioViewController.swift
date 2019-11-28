fe2d7ff77509e217b5a1fe14be3bebd89550ca2c
simranjeetkaur <chakkalsimmi@gmail.com>
2019-11-28 00:25:51 -0500

    adding audio view controller file

---
+++ Group-F-Protective-App-IOS/audioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  audioViewController.swift
+//  Group-F-Protective-App-IOS
+//
+//  Created by Simran Chakkal on 2019-11-28.
+//  Copyright © 2019 Simran Chakkal. All rights reserved.
+//
+
+import UIKit
+
+class audioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
